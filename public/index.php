027c7cf44e1ed7f6cf12899a059f2153250b02cc
cube <asd@asd.com>
2022-06-10 22:57:32 +0500

    Zend framework

--- public/index.php
+++ public/index.php
@@ -1,16 +1,29 @@
 <?php
 
-use Framework\Http\Request;
+
+use Framework\Http\ResponseSender;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
 
 chdir(dirname(__DIR__));
 
 require  'vendor/autoload.php';
 
-$request = new Request();
+$request = ServerRequestFactory::fromGlobals();
 
-//$name =  $_GET['name'] ?? 'Guest';
 $name =  $request->getQueryParams()['name'] ?? 'Guest';
+//echo 'Hello! ' . $name . ' Yo nigga lang: ';
+
+$response = (new HtmlResponse('Hello ' . $name . '!'))
+    ->withHeader('X-Developer', 'ElisDN');
+
+//$response
+$emitter = new Sapi();
+echo $emitter->send($response) . PHP_EOL;
 
 
-header('X-Developer: Safarmurod');
-echo 'Hello! ' . $name . ' Yo nigga lang: ';
+
+
+
+
+
